2695a8a422fded407708cd2dc48b0d90c528aa1c
Max Wilson <io@maxwilson.dev>
2025-08-31 22:41:48 +0000

    feat: starting point with devcontainer

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :tunez, Tunez.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "postgres",
   database: "tunez_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
--- config/test.exs
+++ config/test.exs
@@ -8,7 +8,7 @@ import Config
 config :tunez, Tunez.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "postgres",
   database: "tunez_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
