42f28fcc66dad88675f037a123512e86387fb046
Max Wilson <io@maxwilson.dev>
2025-09-07 18:12:50 +0000

    feat: switch from inline to modular change; feat: end of ch 2

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -18,19 +18,7 @@ defmodule Tunez.Music.Artist do
 
       accept [:name, :biography]
 
-      change fn changeset, _context ->
-               new_name = Ash.Changeset.get_attribute(changeset, :name)
-               previous_name = Ash.Changeset.get_data(changeset, :name)
-               previous_names = Ash.Changeset.get_data(changeset, :previous_names)
-
-               names =
-                 [previous_name | previous_names]
-                 |> Stream.uniq()
-                 |> Enum.reject(fn name -> name == new_name end)
-
-               Ash.Changeset.change_attribute(changeset, :previous_names, names)
-             end,
-             where: [changing(:name)]
+      change Tunez.Music.Changes.UpdatePreviousNames, where: [changing(:name)]
     end
 
     destroy :destroy do
