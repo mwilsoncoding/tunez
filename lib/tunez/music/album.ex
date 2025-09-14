3aba6ee893b02b0abc4e99b688bd5c021cb5e4dd
Max Wilson <io@maxwilson.dev>
2025-09-14 18:50:24 +0000

    task: mix ash.extend Tunez.Music.Album json_api

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -1,5 +1,13 @@
 defmodule Tunez.Music.Album do
-  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+  use Ash.Resource,
+    otp_app: :tunez,
+    domain: Tunez.Music,
+    data_layer: AshPostgres.DataLayer,
+    extensions: [AshJsonApi.Resource]
+
+  json_api do
+    type "album"
+  end
 
   postgres do
     table "albums"
@@ -55,10 +63,10 @@ defmodule Tunez.Music.Album do
     end
   end
 
+  def next_year, do: Date.utc_today().year + 1
+
   identities do
     identity :unique_album_names_per_artist, [:name, :artist_id],
       message: "already exists for this artist"
   end
-
-  def next_year, do: Date.utc_today().year + 1
 end
