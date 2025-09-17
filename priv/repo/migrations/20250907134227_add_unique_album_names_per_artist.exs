d5cc571df864e2ce58f48a640bdcfbe29ea102b7
Max Wilson <io@maxwilson.dev>
2025-09-17 11:46:27 +0000

    fix: typo

--- priv/repo/migrations/20250907134227_add_unique_album_names_per_artist.exs
+++ priv/repo/migrations/20250907134227_add_unique_album_names_per_artist.exs
@@ -1,4 +1,4 @@
-defmodule Tunez.Repo.Migrations.AddJniqueAlbumNamesPerArtist do
+defmodule Tunez.Repo.Migrations.AddUniqueAlbumNamesPerArtist do
   @moduledoc """
   Updates resources based on their most recent snapshots.
 
