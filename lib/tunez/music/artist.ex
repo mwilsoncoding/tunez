59094fc20b3167edd2603121cbbe94cd159b46a2
Max Wilson <io@maxwilson.dev>
2025-09-23 17:51:22 +0000

    feat: document artist resource, its read action, and the argument for querying

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -20,11 +20,18 @@ defmodule Tunez.Music.Artist do
     end
   end
 
+  resource do
+    description "A person or group of people that makes and releases music."
+  end
+
   actions do
     defaults [:read]
 
     read :search do
+      description "List Artists, optionally filtering by name."
+
       argument :query, :ci_string do
+        description "Return only artists with names including the given value."
         constraints allow_empty?: true
         default ""
       end
