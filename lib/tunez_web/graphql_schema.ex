07efea73f1e960efde1476b5f4038d8b8e671a67
Max Wilson <io@maxwilson.dev>
2025-09-27 14:03:54 +0000

    feat: add crud queries to graph_ql block on music domain for artists

--- lib/tunez_web/graphql_schema.ex
+++ lib/tunez_web/graphql_schema.ex
@@ -8,15 +8,15 @@ defmodule TunezWeb.GraphqlSchema do
 
   query do
     # Custom Absinthe queries can be placed here
-    @desc """
-    Hello! This is a sample query to verify that AshGraphql has been set up correctly.
-    Remove me once you have a query of your own!
-    """
-    field :say_hello, :string do
-      resolve fn _, _, _ ->
-        {:ok, "Hello from AshGraphql!"}
-      end
-    end
+    # @desc """
+    # Hello! This is a sample query to verify that AshGraphql has been set up correctly.
+    # Remove me once you have a query of your own!
+    # """
+    # field :say_hello, :string do
+    #   resolve fn _, _, _ ->
+    #     {:ok, "Hello from AshGraphql!"}
+    #   end
+    # end
   end
 
   mutation do
